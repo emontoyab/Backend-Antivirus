d84b8c78a29290a79058b3bd71d2b458930e6d9c
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2025-02-16 17:34:08 -0500

    feat: :sparkles: Implementacion de Crud para Role

---
+++ Antivirus/Controllers/RolesController.cs
@@ -0,0 +1,77 @@
+// RolesController.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Antivirus.Dtos;
+using Antivirus.Services;
+
+namespace Antivirus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleService _roleService;
+
+        public RolesController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoleDto>>> GetAllRoles()
+        {
+            var roles = await _roleService.GetAllRolesAsync();
+            return Ok(roles);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RoleDto>> GetRoleById(long id)
+        {
+            try
+            {
+                var role = await _roleService.GetRoleByIdAsync(id);
+                return Ok(role);
+            }
+            catch (ResourceNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoleDto>> CreateRole(RoleDto roleDto)
+        {
+            var createdRole = await _roleService.CreateRoleAsync(roleDto);
+            return CreatedAtAction(nameof(GetRoleById), new { id = createdRole.Id }, createdRole);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<RoleDto>> UpdateRole(long id, RoleDto roleDto)
+        {
+            try
+            {
+                var updatedRole = await _roleService.UpdateRoleAsync(id, roleDto);
+                return Ok(updatedRole);
+            }
+            catch (ResourceNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(long id)
+        {
+            try
+            {
+                await _roleService.DeleteRoleAsync(id);
+                return NoContent();
+            }
+            catch (ResourceNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Antivirus/services/RoleService.cs
@@ -0,0 +1,63 @@
+// RoleService.cs
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Antivirus.Dtos;
+using Antivirus.Models;
+using Antivirus.Mappers;
+
+namespace Antivirus.Services
+{
+    public class RoleService
+    {
+        private readonly DbContext _context;
+
+        public RoleService(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<RoleDto>> GetAllRolesAsync()
+        {
+            var roles = await _context.Set<role>().ToListAsync();
+            return RoleMapper.MapEntitiesToDtos(roles);
+        }
+
+        public async Task<RoleDto> GetRoleByIdAsync(long id)
+        {
+            var role = await _context.Set<role>().FindAsync(id);
+            if (role == null) throw new ResourceNotFoundException("Role not found");
+            return RoleMapper.MapEntityToDto(role);
+        }
+
+        public async Task<RoleDto> CreateRoleAsync(RoleDto roleDto)
+        {
+            var role = RoleMapper.MapDtoToEntity(roleDto);
+            _context.Set<role>().Add(role);
+            await _context.SaveChangesAsync();
+            return RoleMapper.MapEntityToDto(role);
+        }
+
+        public async Task<RoleDto> UpdateRoleAsync(long id, RoleDto roleDto)
+        {
+            var role = await _context.Set<role>().FindAsync(id);
+            if (role == null) throw new ResourceNotFoundException("Role not found");
+
+            role.name = roleDto.Name;
+            role.trial755 = roleDto.Trial755;
+
+            _context.Set<role>().Update(role);
+            await _context.SaveChangesAsync();
+            return RoleMapper.MapEntityToDto(role);
+        }
+
+        public async Task DeleteRoleAsync(long id)
+        {
+            var role = await _context.Set<role>().FindAsync(id);
+            if (role == null) throw new ResourceNotFoundException("Role not found");
+
+            _context.Set<role>().Remove(role);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
